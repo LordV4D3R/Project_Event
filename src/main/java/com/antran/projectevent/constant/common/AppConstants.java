f4fb86a04c25bf1c83849ad40c67a9389ec6133a
tranan <tranquocan1508@gmail.com>
2024-10-06 19:34:41 +0700

    feat: add Constants

---
+++ src/main/java/com/antran/projectevent/constant/common/AppConstants.java
@@ -0,0 +1,28 @@
+package com.antran.projectevent.constant.common;
+
+public class AppConstants {
+
+    // Error Code
+    public static final int ERROR_CODE = 400;
+
+    // Success Code
+    public static final int SUCCESS_CREATE_CODE = 200;
+    public static final String SUCCESS_CREATE_MESSAGE = "Create data successfully";
+    public static final int SUCCESS_UPDATE_CODE = 200;
+    public static final String SUCCESS_UPDATE_MESSAGE = "Update data successfully";
+    public static final int SUCCESS_DELETE_CODE = 200;
+    public static final String SUCCESS_DELETE_MESSAGE = "Delete data successfully";
+    public static final int SUCCESS_GET_CODE = 200;
+    public static final String SUCCESS_GET_MESSAGE = "Get data successfully";
+
+    // Fail Code
+    public static final int FAIL_CREATE_CODE = 400;
+    public static final String FAIL_CREATE_MESSAGE = "Create data failed";
+    public static final int FAIL_UPDATE_CODE = 400;
+    public static final String FAIL_UPDATE_MESSAGE = "Update data failed";
+    public static final int FAIL_DELETE_CODE = 400;
+    public static final String FAIL_DELETE_MESSAGE = "Delete data failed";
+    public static final int FAIL_GET_CODE = 400;
+    public static final String FAIL_GET_MESSAGE = "Get data failed";
+
+}
